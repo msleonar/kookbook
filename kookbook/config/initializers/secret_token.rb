ff5c8212d8c8ebd68fd87620b4d110334f7be8a8
MIchael <msleonar@macmini.skiptwo.com>
2014-03-04 11:17:48 -0800

    Created a Rails app and added status updates

---
+++ kookbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kookbook::Application.config.secret_key_base = 'ddee8e3bbb19c7934bc755214c370fac46561e5115015151ea7ea4e23d02c42a7041bfe4e9427ffa0e77d475b54ed2ae936a8204131c68b1af67e16a66b2b651'
---
+++ kookbook/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kookbook::Application.config.session_store :cookie_store, key: '_kookbook_session'
